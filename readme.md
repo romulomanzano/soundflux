de73c0f7f0864e17549aa791fd3ccf24208aeef7
romulomanzano <romulomanzano@gmail.com>
2019-01-21 12:06:56 -0500

    Pushing changes to setup instructions

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # Fall detection
+
+## Setup the Pi:
+- Open a terminal (or ssh into your Pi)
+- Create a directory called "/github/falldetection"
+    * mkdir ~/github
+    * mkdir ~/github/falldetection
+    * cd ~/github/falldetection
+* Clone the git repo into it:
+    * git clone https://github.com/romulomanzano/falldetection.git ~/github/falldetection
+* Go to that directory and run the setup_pi script:
+    * cd ~/github/falldetection
+    * ./setup_pi.sh
--- setup_pi.sh
+++ setup_pi.sh
@@ -2,12 +2,8 @@
 sudo apt-get install libasound2
 sudo apt-get install libasound2-dev
 #
-mkdir ~/github
-mkdir ~/github/falldetection
 mkdir ~/github/python_environments
 mkdir ~/github/python_environments/falldetection
-#pull from git
-git clone https://github.com/romulomanzano/falldetection.git ~/github/falldetection
 #create python virtual environment
 python3 -m venv ~/github/python_environments/falldetection
 source ~/github/python_environments/falldetection/bin/active
