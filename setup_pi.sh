e6f570d60c7606e9aece5f74dc077278438fac55
romulomanzano <romulomanzano@gmail.com>
2019-01-24 21:47:47 -0500

    Updates to setup script

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,8 +1,10 @@
 #make sure necessary libraries are there
-sudo apt-get install libasound2
-sudo apt-get install libasound2-dev
-sudo apt install libblas-dev
-sudo apt-get install llvm
+sudo apt install libblas-dev llvm
+sudo apt-get install libatlas-base-dev
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo chmod 600 /var/swap.1
+sudo /sbin/swapon /var/swap.1
 #install virtualenv
 sudo pip3 install virtualenv
 #
@@ -13,3 +15,6 @@ python3 -m venv ~/github/python_environments/falldetection --without-pip
 source ~/github/python_environments/falldetection/bin/activate
 #install requirements
 pip3 install -r requirements.txt --user
+#Revert extra swap
+sudo swapoff /var/swap.1
+sudo rm /var/swap.1
