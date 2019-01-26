084aeac1f0a4a4d93fae892c555cf99759341455
romulomanzano@gmail.com <romulomanzano@gmail.com>
2019-01-26 13:57:20 -0500

    Adding final details for python environment setup

---
+++ tests/test_feature_generation.py
@@ -0,0 +1,22 @@
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+import timeit
+import sys
+
+def test_time_load(file_name='/home/pi/github/falldetection/tests/wav/hello.wav'):
+    start = timeit.default_timer()
+    #loading
+    (rate,sig) = wav.read(file_name)
+    mfcc_feat = mfcc(sig,rate)
+    print(mfcc_feat[1:3,:])
+    #time
+    stop = timeit.default_timer()
+    print('Time with pythonspeech ', stop - start)
+    #less than a second
+    assert stop - start < 1
+
+if __name__ == '__main__':
+    arg = sys.argv[1]
+    test_time_load(arg)
