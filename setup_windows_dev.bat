71d40c5c1d3cd823ad8b87abcb30fc2ca04a3fbc
thielen24 <31086675+thielen24@users.noreply.github.com>
2019-02-13 02:01:36 -0800

    Windows dev setup script and minor changes

--- setup_windows_dev.bat
+++ setup_windows_dev.bat
@@ -1,1 +1,8 @@
-// windows dev environment setup
+mkdir %HOMEDRIVE%%HOMEPATH%\Documents\GitHub\python_environments
+mkdir %HOMEDRIVE%%HOMEPATH%\Documents\GitHub\python_environments\soundflux
+python -m venv %HOMEDRIVE%%HOMEPATH%\Documents\GitHub\python_environments\soundflux
+call %HOMEDRIVE%%HOMEPATH%\Documents\GitHub\python_environments\soundflux\scripts\activate.bat
+pip3 install --upgrade pip --user
+pip3 install -r %HOMEDRIVE%%HOMEPATH%\Documents\GitHub\soundflux\requirements.txt --user
+ECHO "COMPLETE!"
+PAUSE
