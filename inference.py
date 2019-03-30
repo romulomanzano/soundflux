9e43f3dff57cb5a9c24301c963fa586537279c31
romulomanzano <romulomanzano@gmail.com>
2019-03-30 15:22:43 -0400

    Changes to support inference and proper labeling (class level)

--- inference.py
+++ inference.py
@@ -2,14 +2,17 @@ from config import *
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 from keras.preprocessing.image import ImageDataGenerator
-import keras
+from keras.models import model_from_json
+import json
 import utils
 import sys
 
 @utils.logged
 class SoundInference(object):
 
-    def __init__(self, model="VGG16", model_weights_location=SOUNDFLUX_MODEL_WEIGHTS_LOCATION,
+    def __init__(self, model_definition_location=SOUNDFLUX_MODEL_DEFINITION_LOCATION,
+                 model_weights_location=SOUNDFLUX_MODEL_WEIGHTS_LOCATION,
+                 model_labels_location=SOUNDFLUX_MODEL_LABELS_MAP_LOCATION,
                  img_height=80, img_width = 256):
         #prep for inference
         config = tf.ConfigProto()
@@ -18,31 +21,17 @@ class SoundInference(object):
         set_session(sess)
         self.img_height = img_height
         self.img_width = img_width
-        if model == "VGG16":
-            base_model = keras.applications.vgg16.VGG16(weights='imagenet',
-                                                    include_top=False,
-                                                    input_shape=(img_height, img_width, 3))
-            for layer in base_model.layers[:15]:
-                layer.trainable = False
-            """   #Adding custom Layers
-               x = model.output
-               x = Flatten()(x)
-               x = Dense(4096, activation="relu")(x)
-               x = Dropout(0.5)(x)
-               x = Dense(4096, activation="relu")(x)
-               x = Dropout(0.5)(x)
-            """
-            seq_model = keras.models.Sequential()
-            seq_model.add(base_model)
-            seq_model.add(keras.layers.GlobalAveragePooling2D())
-            seq_model.add(keras.layers.Dense(512, activation='relu'))
-            seq_model.add(keras.layers.Dense(64, activation='relu'))
-            seq_model.add(keras.layers.Dense(32, activation='relu'))
-            seq_model.add(keras.layers.Dropout(0.5))
-            seq_model.add(keras.layers.Dense(5, activation='softmax'))
-            self.model = seq_model
-            self.model.load_weights(model_weights_location)
-
+        # load json and create model
+        with open(model_definition_location, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        self.model = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.model.load_weights(model_weights_location)
+        self.logger.info("Loaded model from disk")
+        with open(model_labels_location, 'r') as json_file:
+            label_map = json.load(json_file)
+        self.label_map = label_map
+        self.logger.info("Loaded label mappings: {}".format(self.label_map))
 
     def _get_predict_generator(self, folder):
         datagen = ImageDataGenerator(rescale=1. / 255)
@@ -53,11 +42,22 @@ class SoundInference(object):
 
         return gen
 
+    def _map_inference_to_labels(self,results):
+        map = dict(self.label_map)
+        mapped_results = []
+        for r in results:
+            r_map = {}
+            for k, v in map.items():
+                r_map[k] = r[v]
+            mapped_results.append(r_map)
+        return mapped_results
+
     def predict_img_classes_from_folder(self, folder):
         generator = self._get_predict_generator(folder)
         results = self.model.predict_generator(generator, 4, verbose=True)
-        self.logger.info('Inference results: {}'.format(results))
-        return results
+        mapped_results = self._map_inference_to_labels(results)
+        self.logger.info('Inference results: {}'.format(mapped_results))
+        return mapped_results
 
 
 if __name__ == '__main__':
