0cf2f58d41dee1987f55a8359e04c642b4da90b4
romulo <romulomanzano@gmail.com>
2019-05-04 21:34:42 -0400

    Changes to setup instructions

--- setup_pi_and_respeaker.sh
+++ setup_pi_and_respeaker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "======Setting up SoundFlux application======"
-sh ./setup_pi.sh
+$(source "./setup_pi.sh")
 echo "======Setting up ReSpeaker Mic Array======"
-sh ./setup_respeaker_4.sh
+$(source "./setup_respeaker_4.sh")
